9db005a17291933f2c51664e60ec0729a3840235
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-04 03:25:23 +0530

    Styled

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,51 @@
 import React from 'react'
+import styled from 'styled-components'
+import ChatInput from './ChatInput'
 
 function Chat() {
     return (
-        <div>
-            Chat
-        </div>
+        <Container>
+            <Header>
+                <Group>
+                    <GroupName>
+                        • Group Name
+                    </GroupName>
+                    <GroupInfo>
+                        Encryption: Messages are end-to-end encrypted.
+                    </GroupInfo>
+                </Group>
+            </Header>
+            <MessageContainer></MessageContainer>
+            <ChatInput/>
+        </Container>
     )
 }
 
 export default Chat
+
+const Container = styled.div`
+    display: grid;
+    grid-template-rows: 64px auto min-content;
+    min-height: 0;
+    background-color: #e0eff7; 
+`
+const Header = styled.div`
+    padding-left: 20px;
+    padding-right: 20px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #9fd3ec;
+    justify-content: space-between;
+`
+const Group = styled.div`
+`
+const GroupName = styled.div`
+    font-weight: 700;
+`
+const GroupInfo = styled.div`
+    font-weight: 400;
+    font-size: 13px;
+    margin-top: 8px;
+`
+const MessageContainer = styled.div`
+`
