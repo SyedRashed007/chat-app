3ff86021525ea3de1b2d86065500a8c5cd3209b0
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-05-03 16:33:55 +0530

    Button

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 import { sidebarItems } from '../data/SidebarName'
+import AddCircleTwoToneIcon from '@material-ui/icons/AddCircleTwoTone';
 
 function Sidebar() {
     return (
         <Container>
             <NameContainer>
-                <Name>
                     <h3>Messaging App</h3>
-                </Name>
-                <Logo>
                     <img src="https://img.icons8.com/wired/50/000000/chat.png" alt=""/>
-                </Logo>
             </NameContainer>
             <MainGroups>
+            <AddGroups>
+                Add Group
+            <GroupLogo>
+                <AddCircleTwoToneIcon/>
+            </GroupLogo>
+            </AddGroups>
                 {
                     sidebarItems.map(item => (
                         <ItemContainer>
@@ -40,15 +43,31 @@ const NameContainer = styled.div`
     padding-left: 15px;
     justify-content: space-between;
     border-bottom: 1px solid #11a5ec;
-`
-const Name = styled.div`
-`
-const Logo = styled.div`
-    margin-right: 15px;
-    cursor: pointer;
+
+    img{
+        margin-right: 15px;
+        cursor: pointer;
+    }
 `
 const MainGroups = styled.div`
     padding-top: 20px;
+`
+const AddGroups = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 35px;
+    padding-left: 15px;
+    padding-right: 15px;
+    font-size: x-large;
+`
+const GroupLogo = styled.div`
+    cursor: pointer;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center
 `
 const ItemContainer = styled.div`
     color: #272424;
